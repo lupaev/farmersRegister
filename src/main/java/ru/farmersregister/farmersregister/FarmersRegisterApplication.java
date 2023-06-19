100c196e20570520b718d5439ee13378df6ac842
sergej <lupaevsv@gmail.com>
2023-06-19 12:35:29 +0700

    Initial commit

---
+++ src/main/java/ru/farmersregister/farmersregister/FarmersRegisterApplication.java
@@ -0,0 +1,13 @@
+package ru.farmersregister.farmersregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmersRegisterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FarmersRegisterApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/farmersregister/farmersregister/FarmersRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.farmersregister.farmersregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmersRegisterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
